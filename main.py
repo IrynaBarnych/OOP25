9735086712effafca077fadc49f89f377752830c
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-04 01:28:19 +0200

    четверте завдання

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-class Animal:
-    def __init__(self, name, age, color, weight):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.weight = weight
-    def breath(self):
-        print("дихає")
-    def move(self):
-        print("бігає")
-class Dog(Animal):
-    def voice(self):
-        print("Гавкає!")
-class Cat(Animal):
-            ...
-# Створення екземпляра класу "Собака"
-my_bird = Animal("Вуді", 70, "синьо-білий", 120)
-print(my_bird.name)
-bobik = Dog("Бобініо", 3, "красівий", 60000)
-murchik = Cat("Мурчелло", 10, "срібний табі", 3700)
-print(murchik.color)#атрибут об'єкта
-bobik.weight = 42000
-bobik.height = 60
-print(bobik.height)
-print(bobik.weight)
-# #методи
-bobik.breath()
-bobik.voice()
-murchik.move()
+#Завдання 4
+#Створіть клас Car з атрибутами brand (марка автомобіля), model (модель) та year (рік випуску).
+#Додайте метод start_engine, який виведе повідомлення про те, що двигун запущено.
+
+
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        print("Двигун запущено")
+
+    def car_info(self):
+        print(f"Марка автомобіля: {self.brand}, Модель: {self.model}, Рік випуску: {self.year}")
+
+# Створення екземпляра класу
+car_instance = Car("BMW", "X5", 2022)
+car_instance.start_engine()
+car_instance.car_info()
+
