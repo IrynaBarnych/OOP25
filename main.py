ecd77227c6fe97912e2b229fb511ee1446312f92
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-04 01:41:57 +0200

    п'яте завдання

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-class Animal:
-    def __init__(self, name, age, color, weight):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.weight = weight
-    def breath(self):
-        print("дихає")
-    def move(self):
-        print("бігає")
-class Dog(Animal):
-    def voice(self):
-        print("Гавкає!")
-class Cat(Animal):
-            ...
-# Створення екземпляра класу "Собака"
-my_bird = Animal("Вуді", 70, "синьо-білий", 120)
-print(my_bird.name)
-bobik = Dog("Бобініо", 3, "красівий", 60000)
-murchik = Cat("Мурчелло", 10, "срібний табі", 3700)
-print(murchik.color)#атрибут об'єкта
-bobik.weight = 42000
-bobik.height = 60
-print(bobik.height)
-print(bobik.weight)
-# #методи
-bobik.breath()
-bobik.voice()
-murchik.move()
+# Завдання 5
+# Створіть клас BankAccount з атрибутами balance та owner, а також методами deposit та withdraw для
+# здійснення операцій з балансом. Реалізуйте перевірку на те, що баланс не може стати від'ємним.
+
+
+class BankAccount:
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+        else:
+            print("Сума повинна бути більше 0")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.balance:
+                self.balance -= amount
+            else:
+                print("Недостатньо коштів на рахунку")
+        else:
+            print("Сума повинна бути більше 0")
+
+    def get_balance(self):
+        return self.balance
+
+
+# Створення екземпляра класу
+account = BankAccount(1000, "John")
+account.deposit(500)
+account.withdraw(200)
+print(account.get_balance())
